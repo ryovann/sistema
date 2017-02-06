67a225ba8a2e397f550e9797b7a4c9b8e8f663b0
ryovann <diegodelvalle333@gmail.com>
2017-02-05 22:37:57 -0200

    Sistema
    Falta agregar todo el tema de el porcentaje y el flete

---
+++ Sistema/Backup/Sistema/Cfg.Designer.vb
@@ -0,0 +1,130 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Cfg
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.btnGuardarDatos = New System.Windows.Forms.Button
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox
+        Me.txtPassword = New System.Windows.Forms.TextBox
+        Me.txtUsuario = New System.Windows.Forms.TextBox
+        Me.txtDireccion = New System.Windows.Forms.TextBox
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(6, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(197, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Direccion del servidor de base de datos:"
+        '
+        'btnGuardarDatos
+        '
+        Me.btnGuardarDatos.Location = New System.Drawing.Point(291, 295)
+        Me.btnGuardarDatos.Name = "btnGuardarDatos"
+        Me.btnGuardarDatos.Size = New System.Drawing.Size(75, 23)
+        Me.btnGuardarDatos.TabIndex = 1
+        Me.btnGuardarDatos.Text = "Guardar"
+        Me.btnGuardarDatos.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(6, 57)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(46, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Usuario:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(6, 87)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(64, 13)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Contraseña:"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.txtPassword)
+        Me.GroupBox1.Controls.Add(Me.txtUsuario)
+        Me.GroupBox1.Controls.Add(Me.txtDireccion)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(354, 118)
+        Me.GroupBox1.TabIndex = 4
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Conexion al servidor de base de datos"
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(76, 84)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(139, 20)
+        Me.txtPassword.TabIndex = 6
+        '
+        'txtUsuario
+        '
+        Me.txtUsuario.Location = New System.Drawing.Point(64, 54)
+        Me.txtUsuario.Name = "txtUsuario"
+        Me.txtUsuario.Size = New System.Drawing.Size(151, 20)
+        Me.txtUsuario.TabIndex = 5
+        '
+        'txtDireccion
+        '
+        Me.txtDireccion.Location = New System.Drawing.Point(209, 24)
+        Me.txtDireccion.Name = "txtDireccion"
+        Me.txtDireccion.Size = New System.Drawing.Size(139, 20)
+        Me.txtDireccion.TabIndex = 4
+        '
+        'Cfg
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(378, 330)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.btnGuardarDatos)
+        Me.Name = "Cfg"
+        Me.Text = "Confuguracion del sistema"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents btnGuardarDatos As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents txtPassword As System.Windows.Forms.TextBox
+    Friend WithEvents txtUsuario As System.Windows.Forms.TextBox
+    Friend WithEvents txtDireccion As System.Windows.Forms.TextBox
+End Class
---
+++ Sistema/Backup/Sistema/Cfg.vb
@@ -0,0 +1,34 @@
+﻿Public Class Cfg
+
+    Private Sub btnGuardarDatos_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnGuardarDatos.Click
+        Dim path As String = "C:\Sistema\Data\Config\config.dat"
+        Dim file As System.IO.StreamWriter
+        file = My.Computer.FileSystem.OpenTextFileWriter(path, False)
+        file.WriteLine(txtDireccion.Text)
+        file.WriteLine(txtUsuario.Text)
+        file.WriteLine(txtPassword.Text)
+        file.Flush()
+        file.Close()
+
+
+
+        Inicio.Dir = txtDireccion.Text
+        Inicio.User = txtUsuario.Text
+        Inicio.Pass = txtPassword.Text
+
+        Inicio.Inicio_Load(Nothing, Nothing)
+
+        Me.Visible = False
+
+
+
+
+
+
+    End Sub
+
+  
+    Private Sub Cfg_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
